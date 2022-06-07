2e7a3ad75b4666692b341545d382001c0ac4e127
paichato <tsenane14@gmail.com>
2022-06-08 01:11:27 +0200

    added structure

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+  const id = uuidv4();
+
+  customers.push({ cpf, name, id, statement: [] });
+  return res.status(201).send();
+});
+
+app.listen(3333);
