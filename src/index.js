60914b34273a0e07126e85bdda6df57742fbc282
paichato <tsenane14@gmail.com>
2022-06-08 01:17:56 +0200

    added structure +

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
 
 const customers = [];
 
@@ -10,7 +11,9 @@ app.post("/account", (req, res) => {
   const id = uuidv4();
 
   customers.push({ cpf, name, id, statement: [] });
-  return res.status(201).send();
+  return res.status(201).send(customers);
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("APP is listening on port 3333");
+});
